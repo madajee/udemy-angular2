324afb48c3cd2eecc38027483cf0ae4b4528e873
Jeetan Madaan <madajeeblog@gmail.com>
2020-01-21 16:34:53 +0530

    forth commit: routing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipse.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,9 +10,15 @@ import { RecipeService } from '../recipse.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
-  constructor(private recipseService: RecipeService) { }
+  constructor(private recipseService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipseService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 }
